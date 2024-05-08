a4066d8e2a8539f919ec8ebd4ef0fc120ef2bc1e
amoniw <walkeramoni@yahoo.com>
2024-05-08 13:18:04 -0400

    made changes to hotel

---
+++ src/main/java/Employeeclass.java
@@ -0,0 +1,21 @@
+public class Employeeclass {
+
+}
+
+private int employeeId;
+private  String name;
+private department
+private String payrate;
+private String hoursWorked;
+
+ getPayrate() {
+    return payrate;
+}
+
+ get.RegularHours(String payrate) {
+
+
+  getOvertimeHours() {
+    return Overtimehours;
+}
+
---
+++ src/main/java/reservationclass.java
@@ -0,0 +1,36 @@
+public class reservationclass {
+    }
+
+    public String getRoomType() {
+        String roomType;
+    }
+
+    public void setRoomType(String roomType) {
+    private String; roomType;
+    }
+}
+
+public int getNumberOfNights() {
+    int NumberOfNights;
+}
+
+public void setNumberOfNights(int numberOfNights) {
+    int numberOfNights = numberOfNights;
+}
+
+ public boolean isWeekend() {
+    int isWeekend;
+ }
+
+ public void setWeekend(boolean isWeekend) {
+    this.isWeekend;
+ }
+
+ public static void main(String[] args) {
+  WeekendChecker weekendChecker = new WeekendChecker();
+  System.out.println("Is it the weekend? " + weekendChecker.isWeekend()));
+
+    weekendChecker.setWeekend(true);
+    System.out.println("Is it the weekend" + weekendChecker.isWeekend));
+ }
+
